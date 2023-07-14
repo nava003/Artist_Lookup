d428c8ef468a92cd6ddca61bc9a90b6415fd4286
NavaHub <navaa003@gmail.com>
2023-07-13 20:40:26 -0500

    Checkpoint before main pull

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,1 +1,30 @@
-fetch ('https://www.mediawiki.org/wiki/Special:MyLanguage/API:Query')
+// fetch ('https://www.mediawiki.org/wiki/Special:MyLanguage/API:Query')
+
+// // // // // // // // // // //
+// Alex's Lyric API Codework //
+// var artistName = "Coldplay";
+// var songName = "Adventure of a Lifetime";
+// var requestURL = 'https://api.lyrics.ovh/v1/' + artistName + '/' + songName;
+
+// fetch(requestURL)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//     });
+var request = new XMLHttpRequest();
+
+request.open('GET', 'https://api.lyrics.ovh/v1/Coldplay/Adventure of a Lifetime');
+
+request.onreadystatechange = function () {
+  if (this.readyState === 4) {
+    console.log('Status:', this.status);
+    console.log('Headers:', this.getAllResponseHeaders());
+    console.log('Body:', this.responseText);
+  }
+};
+
+request.send();
+// End Of Alex's Lyric API Codework //
+// // // // // // // // // // // // //
